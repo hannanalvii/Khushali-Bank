811d1e8adb01ba4a69a794094d34b49ab2335166
hannanalvii <hannanalvii@users.noreply.github.com>
2018-11-26 17:50:28 +0500

    recent push

--- app/src/main/java/com/example/android/kb_lms/Main2Activity.kt
+++ app/src/main/java/com/example/android/kb_lms/Main2Activity.kt
@@ -5,8 +5,14 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AppCompatViewInflater
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import java.util.regex.Pattern
 
 class Main2Activity : AppCompatActivity() {
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,10 +21,37 @@ class Main2Activity : AppCompatActivity() {
 
 
     fun secondbtn(view: View){
-         var intent = Intent(this, Dashboard::class.java)
-                 startActivity(intent)
+
+        var email = findViewById<View>(R.id.emailEditText) as EditText
+        var emailText = email.text.toString()
+
+
+
+
+
+        if(android.util.Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
+            var intent = Intent(this, NavDrawer::class.java)
+            startActivity(intent)
+        }
+        else Toast.makeText(this, "enter a valid email", Toast.LENGTH_SHORT).show()
     }
 
 
 
-}
+    }
+
+
+    fun isValid(emailText: String): Boolean {
+        val validEmailAddressRegex = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE)
+        val matcher = validEmailAddressRegex.matcher(emailText)
+        return matcher.find()
+    }
+
+
+
+    fun isEmailValid(email: String): Boolean {
+        return Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE
+        ).matcher(email).matches()
+    }
+
+
